678ce415ba62bbd0a1abfe01934ed07f0036faca
oriano-dev <110156896+oriano-dev@users.noreply.github.com>
2022-11-04 18:19:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const humidity = document.getElementById("humidity");
 const options = {
   method: "GET", // requête HTTP de type 'read' ou lecture
   headers: {
-    "X-RapidAPI-Key": "33a38a48e2msh345936a2215c570p1a003cjsn277d5a16ce04",
-    "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com",
+    "X-RapidAPI-Key": "clé",
+    "X-RapidAPI-Host": "url",
   },
 };
 
